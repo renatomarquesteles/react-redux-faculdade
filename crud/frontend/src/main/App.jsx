5107ca04be0c7c060b98e885d12df461f1b74489
Renato <renatomarquesteles@gmail.com>
2019-10-14 01:53:19 -0300

    tarefa: compras crud

--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -1,20 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'font-awesome/css/font-awesome.min.css'
-import './App.css'
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-import Logo from '../components/template/Logo'
-import Nav from '../components/template/Nav'
-import Routes from './Routes'
-import Footer from '../components/template/Footer'
+import Logo from '../components/template/Logo';
+import Nav from '../components/template/Nav';
+import Routes from './Routes';
+import Footer from '../components/template/Footer';
 
-export default props =>
-    <BrowserRouter>
-        <div className="app">
-            <Logo />
-            <Nav />
-            <Routes />
-            <Footer />
-        </div>
-    </BrowserRouter>
+export default props => (
+  <BrowserRouter>
+    <div className="app">
+      <Logo />
+      <Nav />
+      <Routes />
+      <Footer />
+      <ToastContainer autoClose={3000} />
+    </div>
+  </BrowserRouter>
+);
